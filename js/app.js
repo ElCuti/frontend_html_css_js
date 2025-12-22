1dce84c0018d0893de8560b78f26cfc0ad01db6e
ElCuti <daniel.cutillasc@gmail.com>
2025-12-22 16:41:27 +0100

    Update game size selection default value and initialize input fields in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-alert("Hola");
+const entradaNombre = document.getElementById('nick');
+entradaNombre.value = "Paco";
+console.log(entradaNombre.value);
+
+
+const tamanyoInput = document.getElementById('tamano');
+console.log(tamanyoInput.value)
+console.log(tamanyoInput.options[tamanyoInput.selectedIndex].text)
