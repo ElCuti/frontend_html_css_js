35b8baad2e3772253dbdeb16fa44a2217d189e13
ElCuti <daniel.cutillasc@gmail.com>
2025-12-24 11:02:50 +0100

    Update form action and improve input field styles; add juego.html

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
-const botonJugar = document.getElementById("jugar");
 
-function comprobarForm() {
-    alert("TEST")
-}
-function ejecutarAccion() {
-    alert("Doble TEST")
-    botonJugar.removeEventListener('click', ejecutarAccion);
+// VARIABLES, OBJETOS y DOM
+const nickInput = document.getElementById("nick");
+const tamanoInput = document.getElementById("tamano");
+const formEntrada = document.getElementById("formEntrada");
+
+// FUNCIONES DE EVENTO
+function comprobarForm(event) {
+    // COMPROBAR CAMBIOS
+    if (nickInput.value == "") {
+        alert("FALTA UN NICK");
+        event.preventDefault();
+        nickInput.focus();
+        return false;
+
+    }
+    else if (tamanoInput.value == "0") {
+        alert("FALTA EL TAMAÑO DE JUEGO");
+        event.preventDefault();
+        tamanoInput.focus();
+        return false;
+    }
 }
 
-botonJugar.addEventListener('click', comprobarForm);
-botonJugar.addEventListener('click', ejecutarAccion);
+// INICIO DE CARGA DE EVENTO
+formEntrada.addEventListener('submit', comprobarForm)
